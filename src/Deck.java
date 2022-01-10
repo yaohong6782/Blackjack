e520750ac85143de747b48675bc53fe3c52c4fc9
yaohong <yaohong6782@gmail.com>
2022-01-10 16:14:49 +0800

    game logic implementation, ace values to be reworked

--- src/Deck.java
+++ src/Deck.java
@@ -1,16 +1,16 @@
 import java.util.*;
 
 public class Deck {
-	
+
 	// instance var
 	ArrayList<Card> cards;
-//	static ArrayList<String> val = new ArrayList<>(Arrays.asList("2","3","4","5","6","7","8","9","10", "JACK","QUEEN","KING","ACE"));
-//	static ArrayList<String> sui = new ArrayList<>(Arrays.asList("HEART","SPADE","DIAMOND","CLUB"));
+	//	static ArrayList<String> val = new ArrayList<>(Arrays.asList("2","3","4","5","6","7","8","9","10", "JACK","QUEEN","KING","ACE"));
+	//	static ArrayList<String> sui = new ArrayList<>(Arrays.asList("HEART","SPADE","DIAMOND","CLUB"));
 	// constructor
 	public Deck() {
 		this.cards = new ArrayList<Card>();
 	}
-	
+
 	public void newDeck() {
 		for (Suit s : Suit.values()) {
 			for (Value v : Value.values()) {
@@ -18,13 +18,13 @@ public class Deck {
 			}
 		}
 	}
-	
+
 	public void shuffle() {
 		ArrayList<Card> tempDeck = new ArrayList<>();
 		Random rand = new Random();
 		int randomPos = 0;
 		int originalDeck = this.cards.size(); // 52
-		
+
 		for (int i = 0; i < originalDeck; i++) {
 			// max - min + 1 + min // overflow
 			randomPos = rand.nextInt((this.cards.size()-1-0)+1)+ 0;
@@ -33,7 +33,7 @@ public class Deck {
 		}
 		this.cards = tempDeck; // shuffled  
 	}
-	
+
 	public String toString() {
 		String cardListString = "";
 		for (Card aCard : this.cards) {
@@ -41,22 +41,58 @@ public class Deck {
 		}
 		return cardListString;
 	}
-	
+
 	public void removeCard(int i) {
 		this.cards.remove(i);
 	}
-	
+
 	public Card getCard(int i) {
 		return this.cards.get(i);
 	}
-	
+
 	public void addCard(Card addCard) {
 		this.cards.add(addCard);
 	}
-	
+
 	public void draw(Deck comingFrom) {
 		this.cards.add(comingFrom.getCard(0)); // top of the deck
 		comingFrom.removeCard(0);
 	}
 	
+	public int deckSize() {
+		return this.cards.size();
+	}
+
+	public int cardsValue() {
+		int totalValue = 0;
+		int aces = 0;
+
+		for (Card aCard : this.cards) {
+			switch(aCard.getValue()) {
+			case TWO : totalValue+=2; break;
+			case THREE : totalValue+=3; break;
+			case FOUR : totalValue+=4; break;
+			case FIVE : totalValue+=5; break;
+			case SIX : totalValue+=6; break;
+			case SEVEN : totalValue+=7; break;
+			case EIGHT : totalValue+=8; break;
+			case NINE : totalValue+=9; break;
+			case TEN : totalValue+=10; break;
+			case JACK : totalValue+=10; break;
+			case QUEEN : totalValue+=10; break;
+			case KING : totalValue+=10; break;
+			case ACE : aces+=1; break;
+			}
+		}
+		
+		for (int i = 0; i < aces; i++) {
+			if (totalValue > 10) {
+				totalValue += 1;
+			}
+			else {
+				totalValue += 11;
+			}
+		}
+		return totalValue;
+	}
 }
--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,5 @@
+import java.awt.desktop.SystemSleepEvent;
+import java.util.Scanner;
 
 public class Game {
 
@@ -5,10 +7,59 @@ public class Game {
 		// TODO Auto-generated method stub
 		System.out.println("Game starting");
 		
+		Scanner sc = new Scanner(System.in);
+		
 		Deck playingDeck = new Deck();
 		playingDeck.newDeck();
 		playingDeck.shuffle();
-		System.out.println(playingDeck.toString());
+		
+		Deck playerDeck = new Deck(); // player hand cards
+		
+		Deck dealerDeck = new Deck(); // dealer hand cards 
+		
+		double playerChip = 100.00;
+		
+		while (playerChip > 0) {
+			System.out.println("$" + playerChip + " available, Enter your bet : ");
+			double betPlace = sc.nextDouble();
+			
+			if (betPlace > playerChip) {
+				System.out.println("You're betting more than what you have, please withdraw");
+				break;
+			}
+			
+			playerDeck.draw(playingDeck);
+			playerDeck.draw(playingDeck);
+			
+			dealerDeck.draw(playerDeck);
+			dealerDeck.draw(playerDeck);
+			
+			System.out.println(dealerDeck.getCard(0).toString());
+			while(true) {
+				System.out.println("Your hand currently : ");
+				System.out.println(playerDeck.toString());
+				System.out.println("Your hand current value is : " + playerDeck.cardsValue());
+				
+				System.out.println("Dealer hand : " + dealerDeck.getCard(0).toString() + " and [Unknown]");
+				
+				System.out.println("1. Hit or 2. Stand");
+				int response = sc.nextInt();
+				if (response == 1) {
+					playerDeck.draw(playingDeck); 
+					// Last card draw in array
+					System.out.print("You drew : " + playerDeck.getCard(playerDeck.deckSize()-1)); 
+					
+					// bust
+					if (playerDeck.cardsValue() > 21) {
+						System.out.println("You bust! Hand value : " + playerDeck.cardsValue());
+						playerChip -= betPlace;
+					}
+				}
+				
+			}
+		}
+		
+		System.out.println("Game over, no chips remaining");
 	}
 
 }
